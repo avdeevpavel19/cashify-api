ed0b6b2c3e88f1e4205091091e61bc6fe82c601e
Pavel Avdeev <avdeevpavel19@gmail.com>
2024-01-09 19:26:57 +0300

    create user profile upon registration

--- app/Http/Controllers/Api/v1/Auth/RegisterController.php
+++ app/Http/Controllers/Api/v1/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\v1\Auth\CreateUserRequest;
 use App\Services\Api\v1\Auth\RegisterService;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
--- app/Services/Api/v1/Auth/RegisterService.php
+++ app/Services/Api/v1/Auth/RegisterService.php
@@ -2,12 +2,19 @@
 
 namespace App\Services\Api\v1\Auth;
 
+use App\Models\Profile;
 use App\Models\User;
 
 class RegisterService
 {
     public function store(array $data): User
     {
-        return User::create($data);
+        $user = User::create($data);
+
+        Profile::create([
+            'user_id' => $user->id
+        ]);
+
+        return $user;
     }
 }
